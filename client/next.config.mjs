cf45ed95ddcb39f56fdf50f6424cac611cb5a783
Shoaib2612 <shoaibshoaib2612@gmail.com>
2024-09-25 22:35:46 +0530

    Updated version

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Enables React's strict mode for development
-    swcMinify: true,       // Enables SWC compiler minification for better performance
-    
+    output: 'export',
   };
   
-  export default nextConfig;
+export default nextConfig;
